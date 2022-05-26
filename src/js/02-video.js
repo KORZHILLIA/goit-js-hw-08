bfdc8d9dedbbcb33eea0fb25c7033306dc77697d
KORZH ILLIA <kiev_drum2006@ukr.net>
2022-05-26 11:03:54 +0300

    Task 3 changed

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,14 +1,15 @@
 import Player from '@vimeo/player';
 const throttle = require('lodash.throttle');
 
+const localStorageKey = 'videoplayer-current-time';
 const videoContainer = document.querySelector('#vimeo-player');
 const player = new Player(videoContainer);
-const currentPlayTime = localStorage.getItem('videoplayer-current-time') || 0;
+const currentPlayTime = localStorage.getItem(localStorageKey) || 0;
 
 player.on('timeupdate', throttle(playTimeMemoriser, 1000));
 
 player.setCurrentTime(currentPlayTime);
 
 function playTimeMemoriser({ seconds }) {
-  localStorage.setItem('videoplayer-current-time', seconds);
+  localStorage.setItem(localStorageKey, seconds);
 }
